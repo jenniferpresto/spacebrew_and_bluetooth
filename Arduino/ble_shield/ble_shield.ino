0efa838c87e4f5d1b12f629ea7c83ce9d5bce476
Gustavo Faria <gusfaria11@gmail.com>
2014-03-16 18:25:28 -0400

    Add arduino sketch for BLE Shield

---
+++ Arduino/ble_shield/ble_shield.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <boards.h>
+#include <ble_shield.h>
+#include <services.h>
+
+void setup(){
+  ble_set_name("BLE_NAME");
+
+  //starts the BLE LIBRARY
+  ble_begin(); 
+
+  Serial.begin(57600);
+
+}
+
+void loop(){
+  while ( ble_available() ){
+    Serial.write(ble_read());
+  }
+  while ( Serial.available() ){
+    ble_write('HELLO WORLD');
+    delay(1000);
+  }
+
+  //allows to send and receive data
+  //without this call nothing happens
+  ble_do_events();  
+}
+
