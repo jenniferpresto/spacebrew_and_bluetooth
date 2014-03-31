bc0b5dd2e29987efedc1a57b60b4aab2f02c5431
Gustavo Faria <gusfaria11@gmail.com>
2014-03-31 18:31:58 -0400

    changes to arduino sketch, add chat sketch

--- Arduino/ble_shield/ble_shield.ino
+++ Arduino/ble_shield/ble_shield.ino
@@ -1,10 +1,17 @@
+
+
 #include <SPI.h>
 #include <boards.h>
 #include <ble_shield.h>
 #include <services.h>
 
+const int buttonPin = 2;
+int buttonState = 0;
+
 void setup(){
   ble_set_name("BLE_GUS");
+  
+  pinMode(buttonPin, INPUT);     
 
   //starts the BLE LIBRARY
   ble_begin(); 
@@ -13,17 +20,48 @@ void setup(){
 
 }
 
-void loop(){
+void loop(){  
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {     
+    Serial.println('hello'); 
+  } 
+  else {
+    Serial.println('good bye');
+  }
+
+  
   while ( ble_available() ){
     Serial.write(ble_read());
   }
   while ( Serial.available() ){
-    ble_write('HELLO WORLD');
+    
+    Serial.println('hello world');
+    
+    ble_write(0x48); //H
+    delay(1000); 
+    ble_write(0x65); //e
+    delay(1000);
+    ble_write(0x6C); //l
+    delay(1000);
+    ble_write(0x6C); //l
+    delay(1000);
+    ble_write(0x6F); //o
+    delay(1000);
+    
+    ble_write(0x57); //W
+    delay(1000); 
+    ble_write(0x6F); //o
+    delay(1000);
+    ble_write(0x72); //r
+    delay(1000);
+    ble_write(0x6C); //l
+    delay(1000);
+    ble_write(0x64); //d
     delay(1000);
   }
 
-  //allows to send and receive data
-  //without this call nothing happens
+  
   ble_do_events();  
+  
 }
 
