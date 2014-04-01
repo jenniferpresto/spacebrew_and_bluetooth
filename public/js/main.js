7a71697bbd5fc195228a7a1fe1f726c14b2265a0
Jennifer Presto <sandlappernyc@yahoo.com>
2014-04-01 09:20:23 -0400

    cleaned up comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,12 @@
 /* ****************************
 
 Beginnings of attempt to connect bluetooth serial port module and Spacebrew.
-This file deals only with Spacebrew so far.
+This file so far only receives information via sockets from the web server,
+which has Bluetooth and Spacebrew functionality incorporated.
 
 **************************** */
 
 window.onload = function () {
-
-	// var sb;
-	// var app_name = "Test App";
-
-	// connect to localhost at port 5000
-	// var ws = new WebSocket('ws://127.0.0.1:5000');
-	// console.log(ws);
-
-	// ws.onopen = function () {
-	// 	console.log("Websocket connection opened.");
-	// };
-
-	// ws.onmessage = function(message) {
-	// 	console.log(message);
-	// 	$('body').append("message");
-	// }
 
 	var socket = io.connect(window.location.hostname);
 
@@ -42,35 +27,5 @@ window.onload = function () {
 	socket.on('from spacebrew with love', function(data) {
 		console.log('got spacebrew message: ', data);
 	});
-
-	// function setup() {
-
-	// 	// Spacebrew
-	// 	sb = new Spacebrew.Client({reconnect:true});
-
-	// 	sb.name = app_name;
-	// 	sb.description("Shell application");
-
-	// 	sb.addPublish ( "boolPublisher", "boolean", "false" );
-	// 	sb.addSubscribe ( "boolSubscriber", "boolean" );
-
-	// 	sb.onBooleanMessage = onBooleanMessage;
-	// 	sb.onCustomMessage = onCustomMessage;
-
-	// 	// connect to Spacebrew
-	// 	sb.connect();
-
-	// 	function onBooleanMessage ( name, value ) {
-
-	// 	}
-
-	// 	function onCustomMessage ( name, value, type ) {
-
-	// 	}
-
-	// }
-
-	// // kick things off
-	// setup();
 
 }
